5cfcd50cb922b6e3b6ea5d8004afb2d8a4ab1f1f
Charley Wu <akunzai@gmail.com>
2026-04-08 18:49:07 +0800

    Adapt ServiceLoaderUtil API for log4j2 2.25.x
    ServiceLoaderUtil.loadServices(Class, Lookup) was removed in log4j2 2.25.x.
    Replace with the new ServiceLoaderUtil.safeStream(Class, ServiceLoader, Logger)
    pattern, matching the approach used by log4j2's own SmtpAppender.

--- src/main/java/com/github/akunzai/log4j/SendGridAppender.java
+++ src/main/java/com/github/akunzai/log4j/SendGridAppender.java
@@ -25,10 +25,11 @@ import org.apache.logging.log4j.core.layout.HtmlLayout;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.apache.logging.log4j.core.util.Booleans;
 import org.apache.logging.log4j.core.util.Integers;
+import org.apache.logging.log4j.status.StatusLogger;
 import org.apache.logging.log4j.util.ServiceLoaderUtil;
 
 import java.io.Serializable;
-import java.lang.invoke.MethodHandles;
+import java.util.ServiceLoader;
 
 import static org.apache.logging.log4j.core.layout.AbstractStringLayout.Serializer;
 
@@ -272,8 +273,10 @@ public class SendGridAppender extends AbstractAppender {
                     sandboxMode,
                     bufferSize
             );
-            final ManagerFactory<SendGridManager, FactoryData> factory = ServiceLoaderUtil.loadServices(
-                            SendGridManagerFactory.class, MethodHandles.lookup())
+            final ManagerFactory<SendGridManager, FactoryData> factory = ServiceLoaderUtil.safeStream(
+                            SendGridManagerFactory.class,
+                            ServiceLoader.load(SendGridManagerFactory.class, getClass().getClassLoader()),
+                            StatusLogger.getLogger())
                     .findAny()
                     .orElse(SendGridManager.FACTORY);
             final SendGridManager manager = AbstractManager.getManager(data.managerName, factory, data);
